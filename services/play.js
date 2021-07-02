b1d234bc5c2946148fba3d700c782dd57d40ed38
nicoflores <28nicoooo12@gmail.com>
2021-07-02 14:27:12 -0400

    beta 0.0.6 fix play service

--- services/play.js
+++ services/play.js
@@ -5,7 +5,7 @@ const table = 'plays';
 const updatePlay = async (data) => {
   try {
     const idPlay = await store.get(table, {});
-    console.log(idPlay);
+    console.log(idPlay[0]._id);
     // const editCatalogo = await store.put(
     //     table,
     //     {_id: idPlay},
