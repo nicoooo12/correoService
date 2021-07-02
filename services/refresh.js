bf734d071799acd2bbc614f1623d7fbade3ffe22
nicoflores <28nicoooo12@gmail.com>
2021-07-02 10:11:10 -0400

    beta 0.0.6 fix refresh

--- services/refresh.js
+++ services/refresh.js
@@ -1,17 +1,16 @@
 const axios = require('axios');
-const config = require('../config');
 
 const refresh = async (id) => {
   try {
     if (id) {
       await axios({
         method: 'post',
-        url: `${config.apiUrl}/sockets/updateInfo/${id}`,
+        url: `/sockets/updateInfo/${id}`,
       });
     } else {
       await axios({
         method: 'post',
-        url: `${config.apiUrl}/sockets/updateInfo`,
+        url: `/sockets/updateInfo`,
       });
     }
   } catch (error) {
